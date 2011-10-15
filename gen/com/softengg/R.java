7bca9884558a99e42e919f600cc9b946cbb3b2c6
Saijpaul <nssell2009@gmail.com>
2011-10-15 02:52:22 -0700

    Checking in intial code(SlidingTilePuzzleActivity and EquationBoardActivity) and images

--- gen/com/softengg/R.java
+++ gen/com/softengg/R.java
@@ -11,10 +11,36 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int back1=0x7f020001;
+        public static final int blackback=0x7f020002;
+        public static final int equationbutton=0x7f020003;
+        public static final int equationbutton1=0x7f020004;
+        public static final int exitbutton=0x7f020005;
+        public static final int exitbutton1=0x7f020006;
+        public static final int header=0x7f020007;
+        public static final int helpbutton=0x7f020008;
+        public static final int helpbutton1=0x7f020009;
+        public static final int home=0x7f02000a;
+        public static final int home1=0x7f02000b;
+        public static final int icon=0x7f02000c;
+        public static final int puzzlebutton=0x7f02000d;
+        public static final int puzzlebutton1=0x7f02000e;
+    }
+    public static final class id {
+        public static final int headerLayout=0x7f050001;
+        public static final int imageEquation=0x7f050004;
+        public static final int imageExit=0x7f050008;
+        public static final int imageHelp=0x7f050007;
+        public static final int imageHome=0x7f050002;
+        public static final int imagePuzzle=0x7f050005;
+        public static final int relativeLayoutMain=0x7f050000;
+        public static final int tableRow1=0x7f050003;
+        public static final int tableRow2=0x7f050006;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int equationboard=0x7f030000;
+        public static final int puzzlemain=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/softengg/SlidingTilePuzzleActivity.java
+++ src/com/softengg/SlidingTilePuzzleActivity.java
@@ -1,13 +1,56 @@
+/********************************************************************************
+* Authors: Seema Saijpaul, Pratibha Natani, Neeraja Budamagunta, Maxwell A. Garvey
+*********************************************************************************/
+
 package com.softengg;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SlidingTilePuzzleActivity extends Activity {
+	
+	private ImageView imageEquationMode = null;
+	private ImageView imagePuzzleMode = null;
+	private ImageView imageHelp = null;
+	private ImageView imageExit = null;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.puzzlemain);
+        
+        this.imageEquationMode = (ImageView) this.findViewById(R.id.imageEquation);        
+        this.imagePuzzleMode = (ImageView) this.findViewById(R.id.imagePuzzle);
+        this.imageHelp = (ImageView) this.findViewById(R.id.imageHelp);
+        this.imageExit = (ImageView) this.findViewById(R.id.imageExit);
+        
+        imageEquationMode.setOnClickListener(myListener);
+        imagePuzzleMode.setOnClickListener(myListener);
+        imageHelp.setOnClickListener(myListener);
+        imageExit.setOnClickListener(myListener);
     }
+    
+    View.OnClickListener myListener = new View.OnClickListener(){
+        public void  onClick  (View  v){
+        	switch(v.getId()){
+        	
+	    	case R.id.imageEquation:
+	    		Intent msg = new Intent(SlidingTilePuzzleActivity.this,EquationBoardActivity.class);
+	    		SlidingTilePuzzleActivity.this.startActivity(msg);
+	    	break;
+	    	case R.id.imagePuzzle:
+	    		
+	    	break;
+	    	case R.id.imageExit:
+	    	
+	    		finish();	
+		    break;
+	    	case R.id.imageHelp:
+	   	    	break;
+	   }
+    }};
 }
